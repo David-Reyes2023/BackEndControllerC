bd6612a23ab7a082fea268d69f4b89bc5547c631
David Reyes <david.reyes@tecnologicoliceocristiano.edu.ec>
2024-05-14 19:22:46 -0500

    class commit

---
+++ routes/places-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const placesControllers = require('../controllers/places-controllers');
+
+const router = express.Router();
+
+router.get('/', placesControllers.getAllPlaces);
+
+router.get('/:pid', placesControllers.getPlacesById);
+
+router.get('/users/:uid', placesControllers.getPlacesByUsers);
+
+router.post('/', placesControllers.postPlaces);
+
+router.patch('/:pid', placesControllers.updatePlaces);
+
+module.exports = router;
